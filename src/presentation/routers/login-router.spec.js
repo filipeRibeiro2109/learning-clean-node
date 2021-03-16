ecbfd6663208160162fb0204e499ed9530e0e7ed
Filipe Ribeiro <filipe.parisoto@gmail.com>
2021-03-16 11:46:24 -0300

    ensure LoginRouter if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -8,11 +8,22 @@ class LoginRouter {
   }
 }
 describe('Login Router', () => {
-  test('Shold return 400 if no email is provided', () => {
+  test('Should return 400 if no email is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
