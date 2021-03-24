ce7fdef3d469f9a5eaf8524db26e8ecc091428c3
Filipe Ribeiro <filipe.parisoto@gmail.com>
2021-03-24 14:16:34 -0300

    ensure TokenGenerator return null if JWT returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,17 @@
+const makeSut = () => {
+  class TokenGenerator {
+    async generate (id) {
+      return null
+    }
+  }
+
+  return new TokenGenerator()
+}
+
+describe('Token Generator', () => {
+  test('Should return null if JWT returns null', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
