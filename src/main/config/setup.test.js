335935581534c417240a228a654406ff8140a0c0
Filipe Ribeiro <filipe.parisoto@gmail.com>
2021-03-25 12:52:00 -0300

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,10 +3,20 @@ const app = require('./app')
 
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
-    app.get('/text_x_powered_by', (req, res) => {
+    app.get('/test_x_powered_by', (req, res) => {
       res.send('')
     })
-    const res = await request(app).get('/text_x_powered_by')
+    const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+    const res = await request(app).get('/text_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
